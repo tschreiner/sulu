d9cdb48c0ff63974f99f348d580edd9c53afa9ad
Alexander Schranz <alexander.schranz@massiveart.com>
2015-07-02 22:44:14 +0200

    fix local storage

--- src/Sulu/Bundle/MediaBundle/Media/Storage/LocalStorage.php
+++ src/Sulu/Bundle/MediaBundle/Media/Storage/LocalStorage.php
@@ -50,6 +50,7 @@ class LocalStorage extends AbstractStorage
     {
         $this->storageOptions = new \stdClass();
 
+        $segment = '01';
         if ($preferredStorageOptions) {
             $preferredStorageOptions = json_decode($preferredStorageOptions);
             if (!empty($preferredStorageOptions->segment)) {
